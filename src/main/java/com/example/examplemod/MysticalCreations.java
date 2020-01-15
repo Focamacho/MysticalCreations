f48466c9d44fb3fb3f25946cd872732b8323dfeb
Renan Colombo <renancolombomail@gmail.com>
2020-01-15 07:10:15 -0300

    test

--- src/main/java/com/example/examplemod/MysticalCreations.java
+++ src/main/java/com/example/examplemod/MysticalCreations.java
@@ -1,31 +1,5 @@
 package com.example.examplemod;
 
-import net.minecraft.init.Blocks;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import org.apache.logging.log4j.Logger;
+public class MysticalCreations {
 
-@Mod(modid = MysticalCreations.MODID, name = MysticalCreations.NAME, version = MysticalCreations.VERSION)
-public class MysticalCreations
-{
-    public static final String MODID = "mysticalcreations";
-    public static final String NAME = "Example Mod";
-    public static final String VERSION = "1.0";
-
-    private static Logger logger;
-
-    @EventHandler
-    public void preInit(FMLPreInitializationEvent event)
-    {
-        logger = event.getModLog();
-    }
-
-    @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-        // some example code
-        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
-    }
 }
