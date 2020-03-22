556fd72a7e27da026e31d1053c606551367f8b9d
Renan Colombo <renancolombomail@gmail.com>
2020-03-22 11:04:55 -0300

    Configure dependencies

--- src/main/java/com/focamacho/mysticalcreations/MysticalCreations.java
+++ src/main/java/com/focamacho/mysticalcreations/MysticalCreations.java
@@ -1,16 +1,13 @@
 package com.focamacho.mysticalcreations;
 
-import com.blakebr0.mysticalagriculture.api.MysticalAgricultureAPI;
 import com.focamacho.mysticalcreations.config.ConfigMysticalCreations;
 import com.focamacho.mysticalcreations.events.TooltipEvent;
 import com.focamacho.mysticalcreations.lib.ModCorePlugin;
-import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.ForgeRegistries;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
